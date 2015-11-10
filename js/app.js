e45e5b5ca04b4fe4507c38021fce4919a1d58436
Ivar Lee <ivarr5@gmail.com>
2015-11-10 11:56:44 -0800

    first stage of js chart

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-var comedianStats = function(filepath, comedian) {
+comedianVotes = [];
+
+comedianStats = function(filepath, comedian) {
   this.filepath = filepath;
   this.comedian = comedian;
   this.votes = 0;
+  comedianVotes.push(this); //pushing this object in to comedianVotes
 };
 
 console.log(comedianStats);
@@ -19,7 +22,10 @@ var comedianList = [
   new comedianStats('../images/richard.jpg', 'Richard Pryor'),
   new comedianStats('../images/sarah.jpg', 'Sarah Silverman'),
   new comedianStats('../images/seinfield.jpg', 'Jerry Seinfield')];
+
 console.log(comedianList);
+
+
 
 
 
@@ -29,10 +35,10 @@ var tracker = {
   },
   getImages: function() {
     var imgPath1 = comedianList[this.genRandChoice()].filepath;
-    console.log(imgPath1);
+
 
     var imgPath2 = comedianList[this.genRandChoice()].filepath;
-    console.log(imgPath2);
+
 
     while (imgPath1 === imgPath2) {
       imgPath2 = comedianList[this.genRandChoice()].filepath;
@@ -45,7 +51,38 @@ var tracker = {
     imageFirst.src = imgPath1;
     imageSecond.src = imgPath2;
     return [imgPath1,imgPath2];
-  }
+
+    // var el1 = document.getElementById("image1");
+    // el1.addEventListener("click", tracker.getImages);
+
+  },
+
+
 };
 tracker.getImages();
 
+//chart js
+// var countries= document.getElementById("countries").getContext("2d");
+// new Chart(countries).Pie(pieData, pieOptions);
+// var pieData = [
+//   {
+//     value: 20,
+//     color:"#878BB6"
+//   },
+//   {
+//     value : 40,
+//     color : "#4ACAB4"
+//   },
+//   {
+//     value : 10,
+//     color : "#FF8153"
+//   },
+//   {
+//     value : 30,
+//     color : "#FFEA88"
+//   }
+// ];
+// var pieOptions = {
+//   segmentShowStroke : false,
+//   animateScale : true
+// }
